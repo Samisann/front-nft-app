126a5529afe28bcab58827f39788b50cef098bd2
Sami <sami-la98@hotmail.fr>
2024-05-28 18:34:41 +0200

    Adding files for the assets

---
+++ constants/icons.js
@@ -0,0 +1,11 @@
+import home from '../assets/icons/home.png'
+import game from '../assets/icons/game.png'
+import settings from '../assets/icons/settings.png'
+import play from '../assets/icons/play.png'
+
+export default {
+    home,
+    game,
+    settings,
+    play
+}
