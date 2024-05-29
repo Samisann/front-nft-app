f60f704b11bd8aefa568bed1d6add80a837b4b2b
mahasegni.inbox@gmail.com <MahaSegni>
2024-05-29 10:20:02 +0200

    delete searchBar from home

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -3,7 +3,6 @@ import { SafeAreaView, View, Text, Image, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Link } from 'expo-router';
 import Welcome from '../(auth)/welcome';
-import { SearchBar } from '../(dashboard)/searchBar';
 
 
 export function Home() {
